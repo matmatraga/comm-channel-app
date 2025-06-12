38fd7cfef605e5dd56c17726483c4b9a8f8bc554
matmatraga <matthewmondoy.raga@gmail.com>
2025-06-12 17:01:13 +0800

    fixed CORS

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
 
 // Middlewares
 app.use(cors({
-  origin: ['*'],
+  origin: '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
