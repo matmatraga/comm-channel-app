38324007df2a02f82bac3b4c48a7149dbb519baa
matmatraga <matthewmondoy.raga@gmail.com>
2025-06-13 02:26:13 +0800

    add chat routes to handle file attachment

--- backend/app.js
+++ backend/app.js
@@ -62,6 +62,8 @@ app.use('/api/chats', require('./routes/chatRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/sms', require('./routes/smsRoutes'));
 app.use('/api/voice', require('./routes/voiceRoutes'));
+app.use('/api/chat', require('./routes/chatRoutes'));
+
 
 // Socket.IO Logic
 Socket(io);
